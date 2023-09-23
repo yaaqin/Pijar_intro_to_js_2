61449b42fd3afc26623b215e118e8a2dd764ec92
yaaqin <yaqin.me@gmail.com>
2023-09-23 13:22:44 +0700

    [test]pengetesan perulangan foreach

--- lowercase.js
+++ lowercase.js
@@ -3,4 +3,12 @@ const lastname = "Yaqin";
 const fullname = `${firstname} ${lastname}`
 const result = fullname.toUpperCase()
 
-console.log(result)
+// console.log(result)
+
+const fruits = ['apple', 'banana', 'cherry'];
+
+fruits.forEach(function(fruit, array, test) {
+    const nomor = array + 1
+  console.log(`Fruit at index ${nomor}: ${fruit}`);
+});
+
