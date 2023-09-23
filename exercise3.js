eb38460ea3ff42f03c3176a274bbafed7478f041
yaaqin <yaqin.me@gmail.com>
2023-09-23 15:30:59 +0700

    [ADD]penambahan code exercise 3

--- exercise3.js
+++ exercise3.js
@@ -1,27 +1,77 @@
-function cariDanTampilkanData(nilaiAwal, nilaiAkhir, dataArray) {
-    // Validasi nilaiAwal < nilaiAkhir dan jumlah data dalam dataArray > 5
-    if (nilaiAwal >= nilaiAkhir || dataArray.length <= 5) {
-      console.log("Validasi gagal. Periksa nilaiAwal, nilaiAkhir, atau dataArray.");
-      return;
-    }
-  
-    // Filter data yang sesuai dengan kriteria
-    const hasilPencarian = dataArray.filter((data) => data >= nilaiAwal && data <= nilaiAkhir);
-  
-    // Urutkan hasil pencarian
-    hasilPencarian.sort((a, b) => a - b);
-  
-    // Tampilkan hasil ke layar/console
-    console.log("Data yang sesuai:");
-    hasilPencarian.forEach((data) => {
-      console.log(data);
-    });
+const dataArray = [7, 2, 98, 72, 27, 74, 64];
+const nilaiAwal = 1;
+const nilaiAkhir = 10;
+
+
+
+
+
+
+// cariDanTampilkanData(1,10,dataArray)
+
+const valData1todata2 = (nilai1, nilai2) => {
+  return new Promise ((resolve,reject) => {
+    setTimeout (() => {
+      if (nilai1 < nilai2) {
+        resolve ("nilai 1 dan 2 benar")
+      }else {
+        reject ("Poses gagal")
+      }
+    }, 2500)
+    })
+  }
+
+const cekArr = (arr) => {
+  return new Promise ((resolve,reject) => {
+    setTimeout(() => {
+      if (arr.length >= 5) {
+        resolve("data array benar")
+      } else {
+        reject("Jumlah angka dalam dataArray harus lebih dari 5")
+      }
+    },2000)
+  })
   }
   
-  // Contoh penggunaan fungsi
-  const dataArray = [7, 2, 10, 5, 8, 1, 12, 3, 6, 9];
-  const nilaiAwal = 3;
-  const nilaiAkhir = 11;
-  
-  cariDanTampilkanData(nilaiAwal, nilaiAkhir, dataArray);
-  
+const salahangka = (nilai1,nilai2) => {
+  if (nilai2 <= nilai1) {
+    console.log("nilai ke 2 harus lebih besar dari nilai pertama")
+  }else {
+    return ""
+  }
+}
+
+const cariDanTampilkanData = (nilaiAwal, nilaiAkhir, dataArray) => {
+  return new Promise((resolve, reject) => {
+    setTimeout (() => {
+      const hasilPencarian = dataArray.filter((data) => data >= nilaiAwal && data <= nilaiAkhir);
+      hasilPencarian.sort((a, b) => a - b);
+      const solve = 
+      console.log("Data yang sesuai:")
+      hasilPencarian.forEach((data) => {
+        console.log(data)
+      });
+      resolve(solve)
+      reject("")
+    },3500)
+    })
+  }  
+
+const cekdtaa = async (nilai1,nilai2,nilaiArr) => {
+  try {
+    console.log("pengecekan data...")
+    const array = await cekArr(nilaiArr)
+    const success = await valData1todata2(nilai1,nilai2)
+    const data = await cariDanTampilkanData(nilai1,nilai2,nilaiArr)
+    console.log(success)
+    console.log(array)
+    console.log(data)
+    
+  } catch (error) {
+    console.log(error)
+    const fail = await salahangka(nilai1,nilai2)
+    console.log(fail)
+  }
+}
+
+cekdtaa(1,10,dataArray)
